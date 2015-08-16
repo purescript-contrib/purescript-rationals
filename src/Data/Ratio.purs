778eaf386d1108559463d069bd9511868a503d91
Antti Holvikari <anttih@gmail.com>
2015-08-16 22:50:49 +0300

    Relax constraints a bit

--- src/Data/Ratio.purs
+++ src/Data/Ratio.purs
@@ -10,16 +10,16 @@ instance semiringRatio :: (Semiring a) => Semiring (Ratio a) where
   zero = Ratio zero one
   add (Ratio a b) (Ratio c d) = Ratio ((a * d) + (b * c)) (b * d)
 
-instance ringRatio :: (Semiring a, Ring a) => Ring (Ratio a) where
+instance ringRatio :: (Ring a) => Ring (Ratio a) where
   sub (Ratio a b) (Ratio c d) = Ratio ((a * d) - (b * c)) (b * d)
 
-instance moduloSemiringRatio :: (Ring a, Semiring a, ModuloSemiring a) => ModuloSemiring (Ratio a) where
+instance moduloSemiringRatio :: (Ring a, ModuloSemiring a) => ModuloSemiring (Ratio a) where
   mod a@(Ratio p1 q1) b@(Ratio p2 q2) = a - (Ratio ((p1 * q2) / (p2 * q1)) one) * b
   div (Ratio a b) (Ratio c d) = Ratio (a * d) (b * c)
 
-instance divisionRingRatio :: (Ring a, ModuloSemiring a) => DivisionRing (Ratio a)
+instance divisionRingRatio :: (DivisionRing a) => DivisionRing (Ratio a)
 
-instance numRatio :: (DivisionRing a) => Num (Ratio a)
+instance numRatio :: (Num a) => Num (Ratio a)
 
 numerator :: forall a. Ratio a -> a
 numerator (Ratio a _) = a
